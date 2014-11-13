18fa517c2031abe7b53c7bfe83bb2992ca9aff63
n3rdgir1 <nerdgirl11235813@gmail.com>
2014-11-12 18:53:49 -0600

    init app

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.5'
+gem 'mysql2'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Insider::Application.config.secret_key_base = '8a6a8eb1851107f6de2aaf3971c5b3bd045943075099d2f5dea07af935867739f161776297aeeb646d5927899559baca73d4fb13df02269d361342e29d5cbc21'
